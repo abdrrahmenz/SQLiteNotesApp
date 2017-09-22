1a96be3089601137c0e79f2a7c977885de3d210e
abdrrahmenz <abdrrahmenz@gmail.com>
2017-09-22 08:59:16 +0700

    first commit

---
+++ app/src/main/java/com/expert/andro/sqlitenotesapp/CustomOnItemClickListener.java
@@ -0,0 +1,27 @@
+package com.expert.andro.sqlitenotesapp;
+
+import android.view.View;
+
+/**
+ * Created by adul on 19/09/17.
+ */
+
+public class CustomOnItemClickListener implements View.OnClickListener {
+
+    private int position;
+    private OnItemClickCallback onItemClickCallback;
+
+    public CustomOnItemClickListener(int position, OnItemClickCallback onItemClickCallback) {
+        this.position = position;
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @Override
+    public void onClick(View view) {
+        onItemClickCallback.onItemClicked(view,position);
+    }
+
+    public interface OnItemClickCallback {
+        void onItemClicked(View view, int position);
+    }
+}
